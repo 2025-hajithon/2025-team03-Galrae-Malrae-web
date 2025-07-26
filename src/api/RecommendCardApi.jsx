217a3e81715e67a46748f7fcf1a8a6ef27e1cdd2
tablemin03 <vs201466@naver.com>
2025-07-27 05:31:40 +0900

    feat: Server와 API 연결

--- src/api/RecommendCardApi.jsx
+++ src/api/RecommendCardApi.jsx
@@ -6,17 +6,17 @@ import { mockPlaces } from './TestCards';
 export const RecommendCardApi = () => {
     const [places, setPlaces] = useState(mockPlaces);
 
-  //   useEffect(() => {
-  //   const fetchPlaces = async () => {
-  //     try {
-  //       const response = await apiClient.get('/route/card');
-  //       setPlaces(response.data); 
-  //     } catch (error) {
-  //       console.error("데이터를 불러오는 데 실패했습니다.", error);
-  //     }
-  //   };
-  //   fetchPlaces();
-  // }, []);
+    useEffect(() => {
+    const fetchPlaces = async () => {
+      try {
+        const response = await apiClient.get('/route/card');
+        setPlaces(response.data); 
+      } catch (error) {
+        console.error("데이터를 불러오는 데 실패했습니다.", error);
+      }
+    };
+    fetchPlaces();
+  }, []);
 
   return <RecommendCards places={places} />
 };
