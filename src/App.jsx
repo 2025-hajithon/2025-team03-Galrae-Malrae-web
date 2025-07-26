1264fa98cead64bbd9ab71563870663db0e2ed9c
tablemin03 <vs201466@naver.com>
2025-07-26 23:21:08 +0900

    feat: mypage Router 추가

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import "./App.css";
 import {Home} from "./pages/Home";
 import SignUp from './pages/SignUp';
 import {Setting} from './pages/Setting';
+import {MyPage} from './pages/MyPage';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Route path="/" element={<Home />} />
       <Route path="/signup" element={<SignUp />} />
       <Route path="/setting" element={<Setting />} />
+      <Route path="/mypage" element={<MyPage />} />
     </Routes>
   );
 }
