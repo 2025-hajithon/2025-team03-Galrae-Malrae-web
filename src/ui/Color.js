a82b98965f8d593c3172ac18cbd10a1443304b5c
이호재 <161708856+ghwo336@users.noreply.github.com>
2025-07-26 18:33:18 +0900

    feat: Color 정의

---
+++ src/ui/Color.js
@@ -0,0 +1,14 @@
+const Color = {
+  main: "#F18E2B",
+  white: "#FFFFFF",
+  100: "#F2F2F2",
+  200: "#DDDDDD",
+  400: "#9B9B9B",
+  600: "#6A6A6A",
+  800: "#3B3B3B",
+  black: "#1A1A1A",
+  error: "#FF5555",
+  success: "#1DCE76",
+};
+
+export default Color;
