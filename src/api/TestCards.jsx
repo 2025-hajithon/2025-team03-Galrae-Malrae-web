d5660bd298832eac5ffc5253da5129335a4d9344
tablemin03 <vs201466@naver.com>
2025-07-27 05:02:38 +0900

    feat: 추천지 카드 추가

---
+++ src/api/TestCards.jsx
@@ -0,0 +1,32 @@
+export const mockPlaces = [
+  {
+    "placeName": "경복궁",
+    "date": "2025-08-15",
+    "time": "14:30",
+    "image": "https://picsum.photos/seed/gyeongbokgung/800/600"
+  },
+  {
+    "placeName": "N서울타워",
+    "date": "2025-09-20",
+    "time": "19:00",
+    "image": "https://picsum.photos/seed/nseoultower/800/600"
+  },
+  {
+    "placeName": "DDP (동대문디자인플라자)",
+    "date": "2025-10-05",
+    "time": "16:00",
+    "image": "https://picsum.photos/seed/ddp/800/600"
+  },
+  {
+    "placeName": "롯데월드타워 서울스카이",
+    "date": "2025-11-11",
+    "time": "17:30",
+    "image": "https://picsum.photos/seed/lottetower/800/600"
+  },
+  {
+    "placeName": "코엑스 별마당 도서관",
+    "date": "2025-12-24",
+    "time": "11:00",
+    "image": "https://picsum.photos/seed/coex/800/600"
+  }
+];
