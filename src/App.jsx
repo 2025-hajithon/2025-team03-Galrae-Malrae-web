eda46e3bcbabd15f68703c6495305743bbf4cda9
이호재 <161708856+ghwo336@users.noreply.github.com>
2025-07-26 23:47:14 +0900

    fefe

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import "./App.css";
 import {Home} from "./pages/Home";
 import SignUp from "./pages/SignUp";
 import {Setting} from "./pages/Setting";
+import {Test} from "./pages/Test";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Route path="/" element={<Home />} />
       <Route path="/signup" element={<SignUp />} />
       <Route path="/setting" element={<Setting />} />
+      <Route path="/setting" element={<Test />} />
     </Routes>
   );
 }
